c916122b7706ffd08d8256442a8940c2532e75de
Sukanya Saha <sukanyasaha007@gmail.com>
2021-09-19 23:39:35 -0600

    removed gitignore

--- get_avg_time.py
+++ get_avg_time.py
@@ -4,3 +4,5 @@ import os
 data= pd.read_csv(os.environ.get("LATENCY_REPORT_PATH"),names=['request', 'time(sec)'])
 
 print(data.groupby("request").mean())
+
+#/home/sukanyasaha/distributed-system-assignment2/latencyreport.csv
--- run copy.py
+++
@@ -1,3 +0,0 @@
-from shop import app
-if __name__ =="__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
